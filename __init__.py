d6de22b0f3018ad0bbedeb25fd3ed674a5bb7ab0
carlos1799 <carlosjose1799@gmail.com>
2019-12-17 01:28:09 -0500

    final commit

---
+++ __init__.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask import render_template
+from flask_bootstrap import Bootstrap
+
+app = Flask(__name__)
+Bootstrap(app)
+
+
+@app.route('/')
+def inicio():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug='true')
+
